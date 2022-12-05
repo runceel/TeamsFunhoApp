4b23319eb1165adc6329c145f8a6e28bf362d0d9
Kazuki Ota <kaota@microsoft.com>
2022-12-05 11:04:42 +0900

    multi lines

--- TeamsFunhoApp/Services/FunhoSender.cs
+++ TeamsFunhoApp/Services/FunhoSender.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using AdaptiveCards;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
 using System.Text;
-using System.Threading.Tasks;
 using TeamsFunhoApp.Contracts.Services;
 
 namespace TeamsFunhoApp.Services;
 public class FunhoSender : IFunhoSender
 {
+    private static JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings
+    {
+        NullValueHandling = NullValueHandling.Ignore,
+    };
+
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IAppSettingsService _appSettingsService;
 
@@ -29,13 +32,10 @@ public class FunhoSender : IFunhoSender
             return (false, "メッセージが空です。");
         }
 
+        string json = CreateJson(message);
         var request = new HttpRequestMessage(HttpMethod.Post, _appSettingsService.IncomingWebhookUrl)
         {
-            Content = new StringContent($$"""
-                {
-                    "text": "{{message}}"
-                }
-                """,
+            Content = new StringContent(json,
                 Encoding.UTF8,
                 "application/json"),
         };
@@ -54,5 +54,25 @@ public class FunhoSender : IFunhoSender
         {
             return (false, $"エラーメッセージ: {ex.Message}({ex.StatusCode?.ToString() ?? "None"})");
         }
+    }
+
+    private static string CreateJson(string message)
+    {
+        var normalizedMessage = message.ReplaceLineEndings();
+        var paragraphs = normalizedMessage
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(x => new AdaptiveTextBlock(x));
+        var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+        card.Body.AddRange(paragraphs);
+
+        var attachment = new Attachment
+        {
+            ContentType = "application/vnd.microsoft.card.adaptive",
+            Content = card,
+        };
+
+        var activity = Activity.CreateMessageActivity();
+        activity.Attachments = new List<Attachment> { attachment };
+        return JsonConvert.SerializeObject(activity, JsonSerializerSettings);
     }
 }
